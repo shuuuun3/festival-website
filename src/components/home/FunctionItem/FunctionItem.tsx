3e19775919df51c4a05108f0c5909e5ae8e350f1
Shunsuke <shunsuke.keiko0315@gmail.com>
2025-05-05 20:41:28 +0900

    a

--- src/components/home/FunctionItem/FunctionItem.tsx
+++ src/components/home/FunctionItem/FunctionItem.tsx
@@ -13,11 +13,7 @@ type FunctionItemProps = {
 }
 
 const FunctionItem = forwardRef<HTMLDivElement, FunctionItemProps>(
-<<<<<<< HEAD
   ({ title, icon, href = "", className = ""}, ref) => (
-=======
-  ({ title, icon, href = "", className = "" , scroll}, ref) => (
->>>>>>> 3874ad11fa3fa3618e2657e58826d9bfd12f0a8d
     <div className={`${styles.functionItem} ${className}`} ref={ref}>
       <Link href={href} className={styles.inner} scroll={false}>
         <div className={styles.icon}>
