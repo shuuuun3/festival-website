c5ff5ee41875fd448b2e895af9166992ec999bfd
Shunsuke <shunsuke.keiko0315@gmail.com>
2025-06-10 20:39:56 +0900

    map作成したいなー

---
+++ src/components/map/map.tsx
@@ -0,0 +1,6 @@
+'use client'
+
+import { FC } from "react";
+import * as maplibregl from "maplibre-gl";
+import Map, { ViewState } from "react-map-gl/maplibre";
+import "maplibre-gl/dist/maplibre-gl.css";
