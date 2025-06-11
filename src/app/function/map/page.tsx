5291d943fb701f85b1b7b160ab426ef06415716f
shunsukeHH <shunsuke.keiko0315@gmail.com>
2025-06-11 14:46:28 +0900

    map 表示完了！！

--- src/app/function/map/page.tsx
+++ src/app/function/map/page.tsx
@@ -1,12 +1,32 @@
+"use client";
 
+import maplibregl from 'maplibre-gl';
+import 'maplibre-gl/dist/maplibre-gl.css';
+import React, { useEffect, useRef } from 'react';
 
 export default function Map() {
+  const mapContainer = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!mapContainer.current) return;
+    const map = new maplibregl.Map({
+      container: mapContainer.current,
+      style: '/osm_liberty.json',
+      center: [135.6283103, 34.8481918],
+      zoom: 18.95,
+    });
+
+    // クリーンアップ
+    return () => map.remove();
+  }, []);
+
   return (
     <div>
-      <h1>Map</h1>
-      <p>This is the map page.</p>
+      <div
+        ref={mapContainer}
+        id="map"
+        style={{ width: "100%", height: "100vh" }}
+      />
     </div>
   );
 }
-
-//
